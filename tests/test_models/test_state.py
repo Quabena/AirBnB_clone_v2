b6186f3c4b5b61db820f4251ade7fc139b334d19
JeromeALX <kaphaelghartey@gmail.com>
2025-01-04 20:30:32 +0000

    test

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,50 +1,23 @@
 #!/usr/bin/python3
-"""
-Contains the TestStateDatabaseClass
-"""
-import unittest
+""" """
 import os
+
+from tests.test_models.test_base_model import TestBasemodel
 from models.state import State
-from models import storage
-import MySQLdb
 
 
-class TestStateDatabaseClass(unittest.TestCase):
-    """Test cases for State class with DB storage"""
+class TestState(TestBasemodel):
+    """Represents the tests for the State model."""
+    def __init__(self, *args, **kwargs):
+        """Initializes the test class."""
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                    "Skip if not using db storage")
-    def test_create_state_db(self):
-        """Test creating a state adds a record to database"""
-        # Setup - get initial count
-        conn = MySQLdb.connect(
-            host=os.getenv('HBNB_MYSQL_HOST', 'localhost'),
-            port=3306,
-            user=os.getenv('HBNB_MYSQL_USER'),
-            passwd=os.getenv('HBNB_MYSQL_PWD'),
-            db=os.getenv('HBNB_MYSQL_DB')
+    def test_name3(self):
+        """Tests the type of name."""
+        new = self.value()
+        self.assertEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None)
         )
-        cur = conn.cursor()
-        cur.execute("SELECT COUNT(*) FROM states")
-        initial_count = cur.fetchone()[0]
-        
-        # Action - create new state
-        new_state = State(name="California")
-        new_state.save()
-        
-        # Assert - check count increased
-        cur.execute("SELECT COUNT(*) FROM states")
-        final_count = cur.fetchone()[0]
-        self.assertEqual(final_count, initial_count + 1)
-        
-        # Cleanup
-        cur.close()
-        conn.close()
-
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
-                    "Skip if using db storage")
-    def test_create_state_file(self):
-        """Test creating a state with file storage"""
-        new_state = State(name="California")
-        new_state.save()
-        self.assertIn(new_state, storage.all(State).values())
