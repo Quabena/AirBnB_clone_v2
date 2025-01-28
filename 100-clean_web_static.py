5ce729f2dffe30c85564d25111b5688fd465717c
Evans <ivanz.corbie@gmail.com>
2025-01-28 01:54:03 +0000

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-import os
-from fabric.api import *
+"""
+Fabric script to delete out-of-date archives.
 
-env.hosts = ['100.27.12.226', '100.25.38.188']
+Execute: fab -f 100-clean_web_static.py do_clean:number=<number>
+-i <ssh_key> -u <user>
+"""
+
+from fabric.api import env, local, run
+from os.path import isdir
+env.hosts = ['<IP web-01>', '<IP web-02>']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
     """
-    number = 1 if int(number) == 0 else int(number)
+    Deletes out-of-date archives.
+    :param number: Number of archives to keep.
+    """
+    number = int(number)
+    if number < 1:
+        number = 1
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    # Local cleanup
+    if isdir("versions"):
+        local_archives = sorted(local("ls -tr versions", capture=True).split())
+        archives_to_delete = local_archives[:-number]
+        for archive in archives_to_delete:
+            local("rm -f versions/{}".format(archive))
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    # Remote cleanup
+    remote_path = "/data/web_static/releases"
+    remote_archives = run("ls -tr {}".format(remote_path)).split()
+    remote_archives = [arc for arc in remote_archives if "web_static_" in arc]
+    archives_to_delete = remote_archives[:-number]
+    for archive in archives_to_delete:
+        run("rm -rf {}/{}".format(remote_path, archive))
